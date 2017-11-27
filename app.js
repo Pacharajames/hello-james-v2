0ec16e8e8a8656a7f6fbc0750ac1dc499b39217c
Pachara <pachara@getlinks.co>
2017-11-27 15:14:34 +0700

    v2.3

--- app.js
+++ app.js
@@ -4,4 +4,3 @@ const app = express()
 app.get('/', (req, res) => res.send('Hello World!!!!! JAMESKUNG WTF!!!!!!!!!!!!!!!!!!!!!'))
 
 app.listen(3333, () => console.log('Example app listening on port 3333!'))
-
